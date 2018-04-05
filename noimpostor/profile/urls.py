f1c641784553acfdb620500de5907de44055ee56
the-vampiire <vampiirecodes@gmail.com>
2018-04-05 11:23:17 -0400

    Renamed templates to fit 'user' to 'profile' app name change

--- noimpostor/profile/urls.py
+++ noimpostor/profile/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import profile, update
+from .views import profile
 
 app_name = 'profile'
 
 urlpatterns = [
     path('', profile, name = 'profile'), # username passed from root.urls
-    path('update', update, name = 'update')
+    # path('update', update, name = 'update')
 ]
--- noimpostor/profile/views.py
+++ noimpostor/profile/views.py
@@ -7,7 +7,7 @@ def profile(request, username):
     user = get_object_or_404(User, username = username)
     return render(
         request,
-        'user/profile.html',
+        'profile/index.html',
         context = { 
             'user': user,
             'title': "%s%s's Profile" % (user.username[0].upper(), user.username[1:]) 
