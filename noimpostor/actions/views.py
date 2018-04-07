31c82b1a7e127b8953c38794d3d39a967139ee3d
the-vampiire <vampiirecodes@gmail.com>
2018-04-06 23:41:58 -0400

    Added register action

--- noimpostor/actions/views.py
+++ noimpostor/actions/views.py
@@ -1,7 +1,37 @@
-from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render, reverse, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, reverse, redirect
 from django.contrib.auth import login, logout
+from profile.forms import DefaultPrivacyForm
+from django.contrib.auth.models import User
+from profile.models import DefaultPrivacy
+
+def user_register(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('profile:overview', kwargs = { 'username': request.user.username }))
+
+    register_form = UserCreationForm(data = request.POST or None)
+    privacy_form = DefaultPrivacyForm(data = request.POST or None)
+
+    context = {
+        'title': 'Register',
+        'register_form': register_form,
+        'privacy_form': privacy_form
+    }
+    
+    if request.method == 'POST' and all((register_form.is_valid(), privacy_form.is_valid())):
+        user = register_form.save()
+
+        DefaultPrivacy(
+            user = user,
+            setting = privacy_form.cleaned_data['setting']
+        ).save()
+
+        login(request, user)
+        return redirect(reverse('profile:overview', kwargs = { 'username': user.username }))
+    
+    # GET or invalid form -> re-render the form [blank or with errors]
+    return render(request, 'actions/register.html', context)
 
 def user_login(request):
     # redirect an already logged in user
