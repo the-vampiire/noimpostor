f1bd5110e19ba369562c52d834035d1904ee80a3
the-vampiire <vampiirecodes@gmail.com>
2018-04-07 19:24:33 -0400

    Added Post abstract model and moved Privacy enum
    renamed base_model -> base_models
    - created Post abstract class
    - moved Privacy enum for broader availability

--- noimpostor/profile/models.py
+++ noimpostor/profile/models.py
@@ -1,24 +1,6 @@
 from django.db.models import Model, IntegerField, OneToOneField, ForeignKey, CASCADE
 from django.contrib.auth.models import User
-from root.base_model import Base
-from enum import Enum
-
-class Privacy(Enum):
-    """
-    Privacy settings to be used in the Post and User models as enums
-
-    -1, private: exclusive to the user
-     0, anonymous: in public feed but with no association to user
-     1, public: in public feed with a link to the user profile
-    """
-    private = -1
-    anonymous = 0
-    public = 1
-
-    @classmethod
-    def as_choices(cls):
-        return [(option.value, option.name) for option in cls]
-
+from root.base_models import Base, Privacy
 
 class DefaultPrivacy(Model):
     class Meta:
